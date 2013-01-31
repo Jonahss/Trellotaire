fdeb22f070ff2ab2f528f49fbb597c19a19aa6d4
TinyTimZamboni <jonahss@gmail.com>
2013-01-31 00:50:06 -0500

    clear a new board

--- trellotaire.js
+++ trellotaire.js
@@ -14,13 +14,12 @@ debug = function(o){
 	}
 };
 
-
 ////////////////////
 	
 	
 var robotid = "daveshades";
 var key = "7c4a64bfd8a154965bd35714224a344e";
-var token = "de5e086ed809ae768099b68609ae965487af159faca92f6a95f1469cb5733dbc";
+var token = "d20b755635fd5ac839b8221b366f67356c8f9478a87157409496786d283156ec" //"de5e086ed809ae768099b68609ae965487af159faca92f6a95f1469cb5733dbc";
 
 url.base = {
 	protocol: 'https',
@@ -55,3 +54,31 @@ request(url.build('members/'+robotid+'/notifications'), function(error, response
 		
 	debug(body);
 })
+
+var clear_board = function(){
+
+	var close = function(id){
+		request.put(url.build('lists/'+id+'/closed', {value: true}), function(error, response, body){
+			if (error)
+				console.log(error);
+			console.log('close a list: '+body)
+		});
+	};
+
+	request(url.build('boards/50fdfccd2f15f2f54a000a51/lists'), function(error, response, body){
+		if (error)
+			console.log(error);
+		
+		var lists = JSON.parse(body);
+		debug(body);
+		var list_ids = lists.map(function(x){return x.id});
+		list_ids.forEach(function(id){close(id)});
+	});
+};
+
+clear_board();
+
+
+
+
+
