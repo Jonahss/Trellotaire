58e1f02f0c30a8b10807bee5e05bbc1c3cafaa0a
TinyTimZamboni <jonahss@gmail.com>
2013-02-08 20:28:40 -0500

    card images load! They have been added to an aws S3 bucket. Still lots of work to do.

--- trellotaire.js
+++ trellotaire.js
@@ -21,6 +21,8 @@ debug = function(o){
 };
 
 dots = function(i){
+	if (i == 0)
+		return '0';
 	var arr = new Array(i);
 	for (var x = 0; x < arr.length; x++)
 		arr[x] = '.';
@@ -29,7 +31,7 @@ dots = function(i){
 
 ////////////////////
 		
-var token = "701b343ddb247a907519270814e4a24717e022b63b26ffd451784404965d91b0" //"de5e086ed809ae768099b68609ae965487af159faca92f6a95f1469cb5733dbc";
+var token = "7d5e1479d5443688aa9c21be3b63f2fe1b17b56bb275f4a702c48cccb8a30172" //"de5e086ed809ae768099b68609ae965487af159faca92f6a95f1469cb5733dbc";
 var board = '50fdfccd2f15f2f54a000a51';
 
 url.base = {
@@ -94,18 +96,55 @@ var clear_board = function(callback){
 };
 
 var piles = new Array(7);
+for (var x = 0; x < 7; x++){
+	piles[x] = new Array();
+}
+var pile_ids = new Array(7);
 
-deal = function() {
+pic = function(card){
+	var val = 'default';
+	switch(card.value){
+		case 'J':
+			val = 'Jack';
+			break;
+		case 'Q':
+			val = 'Queen';
+			break;
+		case 'K':
+			val = 'King';
+			break;
+		case 'A':
+			val = 'Ace';
+			break;
+		default:
+			val = card.value;
+	}
+	return "https://s3.amazonaws.com/trellotaire-cards/"+val+"+of+"+card.suit+"s.png";
+}
+
+deal = function(callback) {
+
+	upload_card = function(pile_i){
+		var new_card = deck.draw();
+		request.post(url.build('cards',{name: new_card.toString(), idList: pile_ids[pile_i-1]}), function(error, response, body){
+			console.log('add ' + new_card.toString() + 'to pile ' + pile_i);
+			new_card.id = JSON.parse(body).id;
+			piles[pile_i-1].push(new_card);
+			request.post(url.build('cards/'+new_card.id+'/attachments', {url: pic(new_card), name: new_card.toString()}), function(error, response, body){
+				if (error)
+					console.log(error);
+				debug(body);
+			});
+		});
+	}
+
 	var add_list = function(callback){
 		request.post(url.build('lists', {name: dots(pile_i), idBoard: board, pos:'bottom'}), function(error, response, body){
 			var id = JSON.parse(body).id;
-			piles[pile_i] = id;
+			pile_ids[pile_i-1] = id;
 			
 			for(var card_i = 0; card_i < pile_i; card_i++){
-				var new_card = deck.draw();
-				request.post(url.build('cards',{name: new_card.toString(), idList: id}), function(error, response, body){
-					console.log('add ' + new_card.toString() + 'to pile ' + pile_i);
-				});
+				upload_card(pile_i)
 			}
 			callback();
 		});
@@ -113,16 +152,21 @@ deal = function() {
 	
 	var pile_i = 0;
 	var execute_next = function(){
-		pile_i += 1;
 		console.log("executing next, pile_i= " + pile_i);
-		if (pile_i < 8)
+		pile_i += 1;
+		if (pile_i < 8){
 			add_list(execute_next);
+		} else {
+			callback();
+		}
 	}
 	execute_next();
 }
 
 clear_board(function(){
-		deal();
+	deal(function(){
+		debug(deck);
+	});
 });
 
 
